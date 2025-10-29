c287d7337707c4192f584a36f0a500e893cfbba7
xinetzone <xinzone@outlook.com>
2025-10-29 22:32:35 +0800

    docs(conf.py): 将文档主题从alabaster更改为mystx
    ci(deploy.yml): 添加mystx依赖以支持新文档主题

--- docs/conf.py
+++ docs/conf.py
@@ -89,7 +89,7 @@ blog_feed_templates = {
 disqus_shortname = "https-ablog-readthedocs-io"
 disqus_pages = True
 fontawesome_link_cdn = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-html_theme = "alabaster"
+html_theme = "mystx"
 html_sidebars = {
     "**": [
         "about.html",  # Comes from alabaster
